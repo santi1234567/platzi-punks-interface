468d4e5e78388925cf2233e71571e3c664584b08
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-09-09 16:49:50 -0300

    added homepage and wallet integration

---
+++ src/config/web3/index.js
@@ -0,0 +1,15 @@
+
+import { InjectedConnector } from "@web3-react/injected-connector";
+import Web3 from "web3/dist/web3.min";
+
+const connector = new InjectedConnector({
+  supportedChainIds: [
+    4, // Rinkeby
+  ],
+});
+
+const getLibrary = (provider) => {
+  return new Web3(provider);
+};
+
+export { connector, getLibrary };
