78a3f386f15a817d12cee5781cefe66830501ec5
santi1234567 <45318759+santi1234567@users.noreply.github.com>
2022-10-18 13:52:47 -0300

    Changed chain to gorli

--- src/config/web3/index.js
+++ src/config/web3/index.js
@@ -1,15 +1,15 @@
-
 import { InjectedConnector } from "@web3-react/injected-connector";
 import Web3 from "web3/dist/web3.min";
 
 const connector = new InjectedConnector({
-  supportedChainIds: [
-    4, // Rinkeby
-  ],
+	supportedChainIds: [
+		//4, // Rinkeby
+		5, // Gorli
+	],
 });
 
 const getLibrary = (provider) => {
-  return new Web3(provider);
+	return new Web3(provider);
 };
 
 export { connector, getLibrary };
